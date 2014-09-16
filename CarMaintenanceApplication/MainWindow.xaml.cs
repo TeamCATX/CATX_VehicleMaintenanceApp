3d577df430c8cfc7158852325ca3e3f0923d53c9
mwgolden <matthew@wgolden.com>
2014-09-15 23:24:48 -0500

    initial commit

---
+++ CarMaintenanceApplication/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarMaintenanceApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Add New Car";
+        }
+
+        private void Button_AddCar(object sender, RoutedEventArgs e)
+        {
+            addCarView.Visibility = Visibility.Visible;
+            garageView.Visibility = Visibility.Hidden;
+            reportsView.Visibility = Visibility.Hidden;
+           this.Title = "Add New Car";
+
+        }
+
+        private void Button_Garage(object sender, RoutedEventArgs e)
+        {
+            addCarView.Visibility = Visibility.Hidden;
+            garageView.Visibility = Visibility.Visible;
+            reportsView.Visibility = Visibility.Hidden;
+            this.Title = "My Garage";
+        }
+
+        private void Button_Reports(object sender, RoutedEventArgs e)
+        {
+            addCarView.Visibility = Visibility.Hidden;
+            garageView.Visibility = Visibility.Hidden;
+            reportsView.Visibility = Visibility.Visible;
+            this.Title = "Vehicle Reports";
+        }
+    }
+}
